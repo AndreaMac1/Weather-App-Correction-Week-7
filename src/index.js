c2fdd2b0b99e8793a0aa60229287634e0947cb9f
AndreaMac1 <85474668+AndreaMac1@users.noreply.github.com>
2021-08-18 12:35:04 +0200

    Retrieved coordinates, added API call for daily temp

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -44,6 +45,11 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "aba65921cf9118001ca53f5c5941b98d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -69,6 +75,8 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   //console.log(response.data);
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "aba65921cf9118001ca53f5c5941b98d";
@@ -111,4 +119,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCtemp);
 
 search("New York");
-displayForecast();
