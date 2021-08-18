664a91b681be5a4c73fdaf912fe1488f7176c133
AndreaMac1 <85474668+AndreaMac1@users.noreply.github.com>
2021-08-18 11:26:04 +0200

    Injected HTML to JS for forecast loop

--- src/index.js
+++ src/index.js
@@ -23,6 +23,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img src="media/clear.png" alt="clear" width="50" />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-high"> 18°</span>
+        <span class="weather-forecast-temperature-low"> | 12°</span>
+      </div>
+    </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +111,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCtemp);
 
 search("New York");
+displayForecast();
