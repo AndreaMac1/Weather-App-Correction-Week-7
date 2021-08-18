47e346652dce9d603f6285bce163912619e272a0
AndreaMac1 <85474668+AndreaMac1@users.noreply.github.com>
2021-08-18 13:24:11 +0200

    Added API forecast data

--- src/index.js
+++ src/index.js
@@ -23,24 +23,38 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="media/clear.png" alt="clear" width="50" />
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="${forecastDay.weather[0].main}" width=55 />
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-high"> 18°</span>
-        <span class="weather-forecast-temperature-low"> | 12°</span>
+        <span class="weather-forecast-temperature-high"> ${Math.round(
+          forecastDay.temp.max
+        )}°</span>
+        <span class="weather-forecast-temperature-low"> | ${Math.round(
+          forecastDay.temp.min
+        )}°</span>
       </div>
     </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -74,7 +88,6 @@ function displayTemperature(response) {
     ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  //console.log(response.data);
 
   getForecast(response.data.coord);
 }
